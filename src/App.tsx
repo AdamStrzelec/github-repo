2d393c3943c47f08f2fc4e237b4dcfadbc592646
strzelec <adam.strzelec@pagepro.pl>
2025-05-11 20:59:18 +0200

    install libs

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+}
+
+export default App;
